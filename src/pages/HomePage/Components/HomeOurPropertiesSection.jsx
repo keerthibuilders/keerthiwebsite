7e367fbaa56f865e3284e3b7dc376e5e06767b7b
rkkaushik0894 <rkkaushik0894@gmail.com>
2025-07-31 16:10:07 +0530

    Update HomeOurPropertiesSection.jsx

--- src/pages/HomePage/Components/HomeOurPropertiesSection.jsx
+++ src/pages/HomePage/Components/HomeOurPropertiesSection.jsx
@@ -247,14 +247,12 @@ const HomeOurPropertiesSection = () => {
           <Row className="justify-content-center">
             {properties.map((property) => (
               <Col lg={6} md={6} sm={12} key={property.id} className="mb-4">
-		<a href={property.link}>
                 <PropertyCard
                   image={property.image}
                   title={property.title}
                   location={property.location}
                   video={property.video}
                 />
-		</a>
               </Col>
             ))}
           </Row>
